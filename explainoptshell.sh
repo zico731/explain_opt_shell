f51b02db1058bad0130a63c383b156a32191b042
zico731 <zico731@hotmail.com>
2022-02-23 20:45:07 +0100

    second commit

--- explainoptshell.sh
+++ explainoptshell.sh
@@ -1,2 +1,2 @@
 # explique l'option d'une commande basé sur explainshell.com
-echo "Entrer la commande + option à analyser" ; read -p "commande option : " cmd opt && curl https://explainshell.com/explain?cmd=+${cmd}+-${opt} 2>/dev/null|html2text|awk -e "/^\-${opt}/,/source/"| head -n -1
+echo "Entrer la commande + option à analyser ( ex: ls -l saisir ls l )" ; read -p "commande option : " cmd opt && curl https://explainshell.com/explain?cmd=+${cmd}+-${opt} 2>/dev/null|html2text|awk -e "/^\-${opt}/,/source/"| head -n -1
